40e9ff687c96cf633a08ad90965121045bf33e50
Kyle Gorjanc <kylegorjanc@gmail.com>
2016-12-23 01:50:55 -0500

    added links to bio avatar and h3, adjusted position and margins

--- wp-content/themes/escargatoire/template-parts/biography.php
+++ wp-content/themes/escargatoire/template-parts/biography.php
@@ -10,7 +10,7 @@
 
 <div class="author-info short-bio">
 	<div class="author-description">
-		<?php
+		<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php
 		/**
 		 * Filter the Twenty Sixteen author bio avatar size.
 		 *
@@ -21,14 +21,17 @@
 		$author_bio_avatar_size = apply_filters( 'escargatoire_author_bio_avatar_size', 100 );
 
 		echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
-		?><!-- .author-avatar -->
-		<h3 class="author-title"><span class="author-heading">
+		?></a><!-- .author-avatar -->
+		<h3 class="author-title"><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><span class="author-heading">
 		<!-- <?php // _e( 'Author:', 'escargatoire' ); ?>  -->
-		<?php echo get_the_author(); ?></h3></span>
+		<?php echo get_the_author(); ?></span></a></h3>
 
 		<p class="author-bio">
-			<?php the_author_meta( 'description' ); ?></p>
-			<ul class="bio-links">
+			<?php the_author_meta( 'description' ); ?>
+		</p><!-- .author-bio -->
+	</div><!-- .author-description -->
+	<div class="author-buttons">
+					<ul class="bio-links">
 				<li id="view-all-posts-by"><button class="action-btn author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
 				<?php printf( __( 'All posts by %s', 'escargatoire' ), get_the_author() ); ?></button>
 				</li>
@@ -36,6 +39,5 @@
 				<?php printf( __( 'More from %s', 'escargatoire' ), get_the_author() ); ?></button>
 				</li>		
 			</ul>
-		</p><!-- .author-bio -->
-	</div><!-- .author-description -->
+	</div>
 </div><!-- .author-info -->
